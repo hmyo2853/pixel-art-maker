0696fc302ffd3bec20a76e0c900ef3ce3352a0ac
hmyo2853 <hmyo2853@gmail.com>
2022-11-09 20:55:34 +0900

    canvas 지우고 pixel div 생성 및 레이아웃 잡기(css grid로 grid 생성)

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext("2d");
-const strokeWidth = document.getElementById("line-width");
-const strokeColor = document.getElementById("options_color");
-const clearBtn = document.getElementById("lineAndBtn_clear-btn");
-const saveBtn = document.getElementById("lineAndBtn_save-btn");
-
-const lineText = document.getElementById("lineAndBtn_line-text");
+const container = document.querySelector(".container");
+const sizeEl = document.querySelector(".size");
+const size = sizeEl.value;
+console.log(size);
 
 /////////////// color ///////////////
 const colorArr = [
@@ -21,101 +17,13 @@ const colorArr = [
 ];
 /////////////////////////////////////////
 
-canvas.width = 400;
-canvas.height = 400;
-
-const CANVAS_WIDTH = canvas.width;
-const CANVAS_HEIGHT = canvas.height;
-
-ctx.lineWidth = strokeWidth.value;
-
-let isDrawing = false; // default false
-
-function onMove(event) {
-  if (isDrawing) {
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
-    return;
-  }
-  ctx.moveTo(event.offsetX, event.offsetY);
-}
-
-function nowDrawing() {
-  isDrawing = true;
-}
-
-function endDrawing() {
-  isDrawing = false;
-  ctx.beginPath();
-}
-
-function setLineWidth(event) {
-  ctx.lineWidth = event.target.value;
-  setLineWidthText(event.target.value);
-}
-
-function setLineColor(event) {
-  ctx.strokeStyle = event.target.value;
-}
-
-// line width text change
-function setLineWidthText(x) {
-  if (x.length != 3) {
-    lineText.innerHTML = `굵기 ${x}.0`;
-  } else {
-    lineText.innerHTML = `굵기 ${x}`;
+function divideContainer(s) {
+  container.style.setProperty("--size", s);
+  for (let i = 0; i < s * s; i++) {
+    const div = document.createElement("div");
+    div.classList.add("pixel");
+    container.appendChild(div);
   }
 }
 
-function onClear() {
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
-
-function onSave() {
-  if (confirm("작업한 이미지를 다운로드 하시겠습니까?")) {
-    const imgUrl = canvas.toDataURL();
-    const tagA = document.createElement("a");
-    tagA.href = imgUrl;
-    tagA.download = "pixel-canvas.jpg";
-    tagA.click();
-  }
-}
-
-//////////// colors map ////////////////
-
-const colorOptions = document.getElementById("options_color-options");
-
-colorOptions.innerHTML = colorArr
-  .map((items) => {
-    return (
-      "<div class='option' style='background:" +
-      items +
-      "' id='" +
-      items +
-      "'></div>"
-    );
-  })
-  .join("");
-
-const optionEl = Array.from(document.getElementsByClassName("option"));
-
-///////////////////////////////////////
-
-function setOptionColor(event) {
-  ctx.strokeStyle = event.target.id;
-  strokeColor.value = event.target.id;
-}
-
-canvas.addEventListener("mousemove", onMove);
-canvas.addEventListener("mousedown", nowDrawing);
-canvas.addEventListener("mouseup", endDrawing);
-canvas.addEventListener("mouseleave", endDrawing);
-
-strokeWidth.addEventListener("change", setLineWidth);
-strokeColor.addEventListener("change", setLineColor);
-
-clearBtn.addEventListener("click", onClear);
-saveBtn.addEventListener("click", onSave);
-
-optionEl.forEach((color) => color.addEventListener("click", setOptionColor));
+divideContainer(size);
