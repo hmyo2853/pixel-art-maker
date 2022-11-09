63bc7651b403d0ca0e2a5a3787194b953b07839f
hmyo2853 <hmyo2853@gmail.com>
2022-11-09 22:07:04 +0900

    mouse 이벤트 추가

--- src/app.js
+++ src/app.js
@@ -1,28 +1,42 @@
 const container = document.querySelector(".container");
 const sizeEl = document.querySelector(".size");
 const size = sizeEl.value;
-console.log(size);
+const color = document.querySelector(".colorPicker");
+const resetBtn = document.querySelector(".resetBtn");
 
-/////////////// color ///////////////
-const colorArr = [
-  "#eee8aa",
-  "#ffc0cb",
-  "#FF69B4",
-  "#BEBFD7",
-  "#5A4FCF",
-  "#0060B7",
-  "#31CEFF",
-  "#00BCAF",
-  "#222222",
-];
-/////////////////////////////////////////
+let isDrawing = false;
 
+container.addEventListener("mousedown", () => {
+  isDrawing = true;
+});
+
+container.addEventListener("mouseup", () => {
+  isDrawing = false;
+});
+
+// container를 넘어가면 drawing 취소하기
+window.addEventListener("mousemove", (e) => {
+  if (e.target.className == "pixel" || e.target.className == "container") {
+    return;
+  } else {
+    isDrawing = false;
+  }
+});
+// div pixel 만들기
 function divideContainer(s) {
   container.style.setProperty("--size", s);
   for (let i = 0; i < s * s; i++) {
     const div = document.createElement("div");
     div.classList.add("pixel");
     container.appendChild(div);
+    div.addEventListener("mouseover", () => {
+      if (isDrawing) {
+        div.style.backgroundColor = color.value;
+      }
+    });
+    div.addEventListener("mousedown", () => {
+      div.style.backgroundColor = color.value;
+    });
   }
 }
 
